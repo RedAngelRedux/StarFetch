7258cea67dd41c6a0b14f61dda814a6c5744fb2c
Sammy Nava <sammy@sammynava.com>
2025-08-30 15:44:56 -0500

    Add Movie and MovieList objects based on TMDB's JSON response for 'Now Playing'

---
+++ Models/MovieListResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace StarFetch.Models;
+
+public class MovieListResponse
+{
+    public int Page { get; set; }
+    public List<Movie> Results { get; set; } = [];
+    public int TotalPages { get; set; }
+    public int TotalResults { get; set; }
+}
