16dbe01d14b212cb27512c31b1bc646996b815fc
Sammy Nava <sammy@sammynava.com>
2025-09-05 15:09:57 -0500

    Create Cast Models; Wire & Style <ActorSwiper/>; Position ActorSwiper on MoviesById page

---
+++ Models/MovieCreditsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace StarFetch.Models;
+
+public class MovieCreditsResponse
+{
+    public int Id { get; set; }
+    public List<Cast> Cast { get; set; } = [];
+    public List<Crew> Crew { get; set; } = [];
+}
+
